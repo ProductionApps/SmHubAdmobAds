4b279df8455a14a5161494671a5335fe97a7c340
ankit <ankitkmaurya.iftmu@gmai.com>
2023-09-12 12:22:13 +0530

    interstitial bug fixes

--- smhubadmob/src/main/java/com/smarthub/smhubadmob/ads/admob/AdmobAdController.java
+++ smhubadmob/src/main/java/com/smarthub/smhubadmob/ads/admob/AdmobAdController.java
@@ -453,6 +453,7 @@ public class AdmobAdController{
             });
             return;
         }
+        interstitialListener.onAdDismissed(false);
         if (!isLoading){
             loadInterstitial(id);
         }
